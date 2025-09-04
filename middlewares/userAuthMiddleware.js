01d72c069390acfac5880d9b87d352281026e646
Rudra Pratap Singh <rudrapsingh121@gmail.com>
2025-09-04 21:08:33 +0530

    setup middleware

---
+++ middlewares/userAuthMiddleware.js
@@ -0,0 +1,33 @@
+import { User } from "../models/user";
+import jwt from "jsonwebtoken";
+const userAuthMiddleware = async (req,res,next) => {
+  try
+  {
+    const { token } = req.cookies;
+
+    const jwtPayload =  jwt.verify(token , process.env.JWT_SECRET_KEY)
+
+    const loggedInUser = await User.findById(jwtPayload.id)
+
+    if(!loggedInUser) return res.status(401).json({
+      status : false,
+      message : "User doesn't exist"
+    })
+
+    req.user = loggedInUser;
+
+    next()
+  }
+  catch(error)
+  {
+    res.status(401).json(
+      {
+        status : false,
+        message : error.message,
+      }
+    )
+  }
+
+}
+
+export default userAuthMiddleware;
