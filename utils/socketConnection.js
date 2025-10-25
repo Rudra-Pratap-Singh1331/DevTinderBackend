7ae10d05e53d3838b0b00199a937b3213ef53f2d
Rudra Pratap Singh <rudrapsingh121@gmail.com>
2025-10-25 11:56:03 +0530

    build rela time chat features

--- utils/socketConnection.js
+++ utils/socketConnection.js
@@ -1,14 +1,32 @@
 import { Server } from "socket.io";
+import Chat from "../models/ChatModel.js";
 
 const socketConnection = (server) => {
   const io = new Server(server, {
     cors: {
       origin: "http://localhost:5173", // your frontend origin
+      credentials: true,
     },
   });
 
   io.on("connection", (socket) => {
     //handle events here
+    socket.on("joinChat", ({ userId, _id }) => {
+      const roomId = [userId, _id].sort().join("@");
+      console.log(roomId);
+      socket.join(roomId);
+    });
+    socket.on("sendMessage", async ({ userId, _id, text , time}) => {
+      const newChat = new Chat({
+        fromUserId: userId,
+        toUserId: _id,
+        text: text,
+      });
+      await newChat.save();
+
+      const roomId = [userId, _id].sort().join("@");
+      socket.to(roomId).emit("messageReceived", { text , time , userId});
+    });
   });
 };
 
